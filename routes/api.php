edca3481da3926ebfe8fed02ca3cb692e738bd07
unknown <raj.kumar@anviam.com>
2022-09-03 18:59:13 +0530

    fix

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', [App\Http\Controllers\ChatController::class, 'index'])->middleware('api')->name('home');
+Route::get('/users', [App\Http\Controllers\ChatController::class, 'index'])->middleware('auth:api')->name('home');
 
 Route::post('register',[AuthenticationController::class,'registerUserExample']);
 Route::post('login',[AuthenticationController::class,'loginUserExample']);
